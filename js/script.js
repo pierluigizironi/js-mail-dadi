581bf0b8afb3cbb9bad49ec135264099b470ffdb
pierluigizironi <zironipierluigi@gmail.com>
2022-01-10 15:18:33 +0100

    add mail arrays

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ console.log('JS OK')
 // Faccio inserire all'utente la propria email
 // Attraverso l'input che viene dato controllo se fa parte di quelle registrate
 // Do una riposta in base alla corrispondenza tra mail inserita e mail presenti
+
+
+// Inizio creando una serie di mail
+const emails = ['marco@libero.it','stefano@gmail.com', 'marcello@outlook.com', 'cassandra@gmail.com' ]
+
+// Faccio inserire all'utente la propria email
