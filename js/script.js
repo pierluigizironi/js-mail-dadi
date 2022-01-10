07fb154d5023f5232f4b91e27a29ec1b5776c9a5
pierluigizironi <zironipierluigi@gmail.com>
2022-01-10 15:10:09 +0100

    add human coding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+// # Mail
+//Crea un array di email;
+//Chiedi all’utente la sua email.
+//controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+
+// Inizio creando una serie di mail
+// Faccio inserire all'utente la propria email
+// Attraverso l'input che viene dato controllo se fa parte di quelle registrate
+// Do una riposta in base alla corrispondenza tra mail inserita e mail presenti
