09e3140b80aae7c9a180f7ab37a447544b71eccd
pierluigizironi <zironipierluigi@gmail.com>
2022-01-10 16:37:00 +0100

    add human coding

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Stampare in console i tiri di dado e il risultato.
+
+// Metto due const dove riesco a generare due numeri randomici da 1 e 6
+// Inizio a decretare il vincitore in usando le if clauses
+//Stampo con console.log
