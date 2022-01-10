d2c1386e59b572c74e2d23ea390f58eaba836f7d
pierluigizironi <zironipierluigi@gmail.com>
2022-01-10 16:47:19 +0100

    add const and if clauses

--- js/script2.js
+++ js/script2.js
@@ -5,3 +5,20 @@
 // Metto due const dove riesco a generare due numeri randomici da 1 e 6
 // Inizio a decretare il vincitore in usando le if clauses
 //Stampo con console.log
+
+// Metto due const dove riesco a generare due numeri randomici da 1 e 6
+const userNumber = Math.floor( Math.random() * 6 ) +1;
+console.log('Numeri giocatore:', userNumber)
+const pcNumber = Math.floor( Math.random() * 6 ) +1;
+console.log('Numeri computer:', pcNumber)
+
+// Inizio a decretare il vincitore in usando le if clauses
+//Stampo con console.log
+
+if (userNumber > pcNumber) {
+    console.log('Congratulazioni! Hai vinto')
+} else if (userNumber == pcNumber) {
+    console.log('Pareggio! Ritenta!')
+} else {
+    console.log('Hai perso!')
+}
